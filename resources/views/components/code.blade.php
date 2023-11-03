718eff30ee16e4967ec09fae72c43bf1c72b93d7
Francisco Madeira <xico2k@gmail.com>
2023-11-03 21:17:45 +0000

    feat: Allow multiple lines

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -3,16 +3,20 @@
     'letters'
 ])
 
-<div {{ $attributes->class(['text-center flex flex-wrap gap-8 mt-auto']) }}>
-    @foreach(array_filter(explode(' ', trim($message))) as $word)
-        <div class="word flex flex-wrap gap-2">
-            @foreach(str_split($word) as $character)
-                <div>
-                     <span class="material-symbols-outlined character !text-4xl">
-                        {{ $letters[strtolower($character)] ?? $character }}
-                    </span>
+<div {{ $attributes->class(['text-center mt-auto flex flex-wrap flex-col gap-8']) }}>
+    @foreach(array_filter(explode("\n", trim($message))) as $line)
+        <div class="line flex flex-wrap gap-8">
+            @foreach(explode(' ', trim($line)) as $word)
+                <div class="word flex flex-wrap gap-2">
+                    @foreach(str_split($word) as $character)
+                        <div>
+                             <span class="material-symbols-outlined character !text-4xl">
+                                {{ $letters[strtolower($character)] ?? $character }}
+                            </span>
 
-                    <div class="bg-gray-200 w-12 h-12"></div>
+                            <div class="bg-gray-200 w-12 h-12"></div>
+                        </div>
+                    @endforeach
                 </div>
             @endforeach
         </div>
