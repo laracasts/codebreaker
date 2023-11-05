0c6b67bf776e6be7561819551ce69b24c5d06e24
Kay Markschies <kayschima@web.de>
2023-11-05 14:59:51 +0100

    debounce input to save some requests

--- resources/views/livewire/create-code.blade.php
+++ resources/views/livewire/create-code.blade.php
@@ -1,6 +1,6 @@
 <div class="my-8 flex flex-col items-center gap-10 flex-1">
     <form class="print:hidden w-full">
-        <textarea wire:model.live="message" cols="30" rows="10"
+        <textarea wire:model.live.debounce.250ms="message" cols="30" rows="10"
                   class="bg-white/10 text-white py-2 px-3 rounded-xl w-full"></textarea>
 
         <div class="mt-2 flex justify-end">
