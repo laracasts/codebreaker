5928193e285b7314738f0dc028e74c5081797057
Marco "Mint" Introini <mint.dev@pm.me>
2023-11-19 09:20:21 +0100

    Don't show bottom squares with symbols

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -12,7 +12,9 @@
                         {{ $letters[strtolower($character)] ?? $character }}
                     </span>
 
-                    <div class="bg-gray-200 w-12 h-12"></div>
+                    @if(preg_match("/[a-z]/i", $character))
+                        <div class="bg-gray-200 w-12 h-12"></div>
+                    @endif
                 </div>
             @endforeach
         </div>
